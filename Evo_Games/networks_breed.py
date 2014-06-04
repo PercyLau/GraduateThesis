10a8d716645922b96e6104056b851cb610c1a4e2
七饭鸟 <lockheedphoenix@outlook.com>
2014-06-04 15:16:00 +0800

    alpha edition
    alpha edition

--- Evo_Games/networks_breed.py
+++ Evo_Games/networks_breed.py
@@ -8,19 +8,7 @@ E-mail:lockheedphoenix@gmail.com
 Created on: 3/13/14 9:06 PM
 
 Copyright (C)  lockheedphoenix
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or  any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 """
 
 import graph_tool.all as gt
